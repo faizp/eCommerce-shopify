9aaf1456f0e03e96fb3c2797bd7608a208363b37
Faiz P <faizkizhisseri@gmail.com>
2021-02-03 16:25:19 +0530

    database postgre

--- shopify/settings.py
+++ shopify/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'shopify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
