cee9b8ed4df411dc1847a8099ff8bbd1e71e559d
Faiz P <faizkizhisseri@gmail.com>
2021-02-09 01:12:30 +0530

    before merge

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from . forms import UserRegisterForm
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 from shop.models import Product
 from django.contrib.auth import login, authenticate, logout
 
@@ -12,30 +13,36 @@ def index(request):
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(index)
-    else:
-        form = UserRegisterForm()
-    return render(request, 'user/register.html', {"form": form})
-
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return JsonResponse('true', safe=False)
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(index)
         else:
-            return JsonResponse('false', safe=False)
+            form = UserRegisterForm()
+        return render(request, 'user/register.html', {"form": form})
     else:
-        return render(request, 'user/login.html')
+        return redirect(index)
+
+@csrf_exempt
+def login_user(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return JsonResponse('true', safe=False)
+            else:
+                return JsonResponse('false', safe=False)
+        else:
+            return render(request, 'user/login.html')
+    else:
+        return redirect(index)
 
 
-def logout(request):
+def logout_user(request):
     logout(request)
-    return redirect(login)
+    return redirect(index)
