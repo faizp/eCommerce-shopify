f071ad9b2b3ddbeb88032c6e8a44776ff1e55dbb
Faiz P <faizkizhisseri@gmail.com>
2021-02-13 10:44:40 +0530

    image preview when add

--- static/js/script.js
+++ static/js/script.js
@@ -49,3 +49,20 @@ function addQuantiy(id) {
     })
 
 }
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#image1').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#image1").change(function(){
+        readURL(this);
+    });
+
